83e8a22d66a0fa2931b955ccac588645db4394e0
AntonioAlvarado21 <jcruza1300@alumno.ipn.mx>
2021-10-29 11:15:26 -0500

    API Restfull - Finalizado

--- src/main/java/com/notas/core/controller/ClienteRest.java
+++ src/main/java/com/notas/core/controller/ClienteRest.java
@@ -1,17 +1,40 @@
 package com.notas.core.controller;
 
+import com.notas.core.entity.Nota;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/nota")
 public class ClienteRest {
-
-	@GetMapping("/all")
-	public String devolvertodos()
-	{
-		return "template";
-	}
-	
+    
+    private final String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJkYXZpZGJyaW9uZXMifQ.74o-HxtdCtSv4HD6ADn7TgLv2qUDdESR5J_kk8FdGTo43psguoL6Pi37ktuPlJSrRXhkUFIQvxaN0pX4cLvsww";
+    
+    @GetMapping("/all")
+    public ModelAndView devolvertodos(){
+        ModelAndView mav = new ModelAndView("template");
+        
+        RestTemplate rest = new RestTemplate();
+        
+        HttpHeaders headers = new HttpHeaders();
+        
+        headers.add("Authorization", token);
+        
+        HttpEntity entity = new HttpEntity(headers);
+        
+        ResponseEntity<Nota[]> notasEntity = rest.exchange("http://localhost:8070/v1/notas",HttpMethod.GET,entity, Nota[].class);
+        
+        Nota[] notas = notasEntity.getBody();
+        
+        mav.addObject("notas",notas);
+        return mav;
+    }
+    
 }
